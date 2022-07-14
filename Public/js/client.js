58d2533abd34a197abee0e3689f723952ec4d10b
Youssef Gamal <youssefgamal56@gmail.com>
2022-07-14 17:31:25 +0200

    latest updates

--- Public/js/client.js
+++ Public/js/client.js
@@ -1,36 +1,6 @@
 document.getElementById("getInfo1").addEventListener("click", myFunction1);
 document.getElementById("getInfo2").addEventListener("click", myFunction2);
 
-document.getElementById("hideInfo1").addEventListener("click", hideFunction1);
-document.getElementById("hideInfo2").addEventListener("click", hideFunction2);
-
-function hideFunction1(){
-    document.getElementById("info1").innerHTML = ` `;
-}
-function hideFunction2(){
-    document.getElementById("info2").innerHTML = ` `;
-}
-
-const getDeviceInformation = (deviceId) =>
-    fetch(`http://localhost:4041/iot/devices/${deviceId}`, {
-        method: "GET",
-        mode: "cors",
-
-        headers: {
-            "fiware-service": "openiot",
-            "fiware-servicepath": "/",
-            "content-Type": "application/json",
-        },
-    })
-    .then((res) => {
-        if (res.status === 200) {
-            return res.json();
-        } else {
-            throw new Error(res.statusText);
-        }
-    })
-    .then((data) => data);
-
 
 const getDeviceReading = (deviceName) => {
     const headers = new Headers();
@@ -52,6 +22,7 @@ const getDeviceReading = (deviceName) => {
 
     ;
 }
+
 const getActuatorState = (ActuatorID) => {
     const headers = new Headers();
     headers.set('fiware-service', 'openiot')
@@ -74,12 +45,9 @@ const getActuatorState = (ActuatorID) => {
 }
 
 const getEntityInfo = (EntityName) => {
-    const headers = new Headers();
-    headers.set('Content-Type', 'application/json')
     return fetch(`http://localhost:1026/v2/entities/${EntityName}`, {
             method: "GET",
             mode: 'cors',
-            //headers
         })
         .then((res) => {
             if (res.status === 200) {
@@ -92,7 +60,6 @@ const getEntityInfo = (EntityName) => {
     ;
 }
 
-
 async function myFunction1() {
     const data = await getEntityInfo("urn:ngsi-ld:Greenhouse:001");
     document.getElementById("info1").innerHTML = 
@@ -103,7 +70,11 @@ async function myFunction1() {
     <h4>Address Locality : </h4><span>${data['address']['value']['addressLocality']}</span><br>
     <h4>Country : </h4><span>${data['address']['value']['addressCountry']}</span><br>
     <h4>Street Address : </h4><span>${data['address']['value']['streetAddress']}</span><br>
-    <h4>Location Coordinates : </h4><span>${data['location']['value']['coordinates']}</span></div>`;
+    <h4>Location Coordinates : </h4><span>${data['location']['value']['coordinates']}</span><br>
+    <button class="main-filled-button" id="hideInfo1" style="margin-top:10px;">Hide Information</button></div>`;
+    document.getElementById("hideInfo1").addEventListener("click", ()=>{
+        document.getElementById("info1").innerHTML = ` `;
+    });
 };
 
 async function myFunction2() {
@@ -116,7 +87,11 @@ async function myFunction2() {
     <h4>Address Locality : </h4><span>${data['address']['value']['addressLocality']}</span><br>
     <h4>Country : </h4><span>${data['address']['value']['addressCountry']}</span><br>
     <h4>Street Address : </h4><span>${data['address']['value']['streetAddress']}</span><br>
-    <h4>Location Coordinates : </h4><span>${data['location']['value']['coordinates']}</span></div>`;
+    <h4>Location Coordinates : </h4><span>${data['location']['value']['coordinates']}</span><br>
+    <button class="main-filled-button" id="hideInfo2" style="margin-top:10px;">Hide Information</button></div>`;
+    document.getElementById("hideInfo2").addEventListener("click", ()=>{
+        document.getElementById("info2").innerHTML = ` `;
+    });
 }
 
 // Get the sensors reading every 5 seconds and display it 
